d7ea68dc55fa330c756ff7b5caaae6c59c0fadcc
ericpkatz <ericpkatz@gmail.com>
2023-03-20 19:43:05 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import Home from './Home';
--- src/store.js
+++ src/store.js
@@ -101,6 +101,6 @@ export const updateThing = (thing)=> {
 };
 
 
-const store = createStore(reducer, applyMiddleware(logger, thunk));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
